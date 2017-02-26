5e96aa6c30ac88a03087d27dd4c5be8f0411f7fb
D.A.Bashkirtsevich <bashkirtsevich@users.noreply.github.com>
2017-02-26 10:55:39 +0500

    Update face_detect_cv3.py
    Add mouth and eyes detection

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -2,11 +2,15 @@ import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+imagePath = 'python.jpg'
+cascPath = 'haarcascade_frontalface_default.xml'
+smilePath = 'haarcascade_smile.xml'
+eyePath = 'haarcascade_eye.xml'
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+smileCascade = cv2.CascadeClassifier(smilePath)
+eyeCascade = cv2.CascadeClassifier(eyePath)
 
 # Read the image
 image = cv2.imread(imagePath)
@@ -15,10 +19,10 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
+    scaleFactor=1.05,
+    minNeighbors=8,
+    minSize=(55, 55)
+    #flags=cv2.CV_HAAR_SCALE_IMAGE
 )
 
 print("Found {0} faces!".format(len(faces)))
@@ -26,6 +30,31 @@ print("Found {0} faces!".format(len(faces)))
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
+
+    smile = smileCascade.detectMultiScale(
+        roi_gray,
+        scaleFactor=1.7,
+        minNeighbors=2,
+        minSize=(25, 25)
+    )
+
+    # Set region of interest for smiles
+    for (x, y, w, h) in smile:
+        print "Found", len(smile), "smiles!"
+        cv2.rectangle(roi_color, (x, y), (x+w, y+h), (255, 0, 0), 1)
+
+    eyes = eyeCascade.detectMultiScale(
+        roi_gray#,
+        #scaleFactor=1.7,
+        #minNeighbors=1,
+        #minSize=(17, 17)
+    )
+    for (ex,ey,ew,eh) in eyes:
+        print "Found", len(eyes), "eyes!"
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),1)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
